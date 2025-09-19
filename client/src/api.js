d170d0c47d5ccb9f7698f9ef40e552e9d70b82c5
srishti0x <vermasrishti1508@gmail.com>
2025-09-19 21:05:36 +0530

    Update API base URL to Render backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000", // Backend URL
+  baseURL: "https://dailybrief-b1ud.onrender.com", // Backend URL
 });
 
 // All news
